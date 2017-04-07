475cd8381d14185047606c1b29ea1c03f7865163
ariel.duarte <arielpy@gmail.com>
2017-04-07 11:01:02 -0300

    update server port

--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 
 var mongoose = require('mongoose');
 var app = require('./app');
-var port = process.env.PORT || 3000;
+var port = process.env.PORT || 1245;
 
 
 // MongoDb connection with mongoose
